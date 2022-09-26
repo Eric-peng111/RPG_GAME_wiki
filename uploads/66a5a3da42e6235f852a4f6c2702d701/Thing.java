7df0d480d3bbbf76818d51ed38faed4b9d9d3205
Xuan Ngo <u7306766@anu.edu.au>
2022-09-26 23:37:21 +1000

    Upload attachment Thing.java

---
+++ uploads/66a5a3da42e6235f852a4f6c2702d701/Thing.java
@@ -0,0 +1,36 @@
+/*
+ * Bitwise Books & Courses - sample Java code
+ * http://www.bitwisebooks
+ * http://www.bitwisecourses.com
+ */
+
+package gameobjects;
+
+public class Thing {
+     // Basic Thing type that defines all objects in the Adventure
+
+    private String name;
+    private String description;
+
+    public Thing(String aName, String aDescription) {
+        // constructor
+        name = aName;
+        description = aDescription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String aDescription) {
+        description = aDescription;
+    }
+}
